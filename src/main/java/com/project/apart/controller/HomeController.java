3c23e01f944ed9f089a06482205ace13bdcd71cd
onlyj <tlawltjr23@gmail.com>
2023-06-06 14:57:09 +0900

    프로퍼티스 수정, index 컨트롤러

---
+++ src/main/java/com/project/apart/controller/HomeController.java
@@ -0,0 +1,14 @@
+package com.project.apart.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("")
+    public String index() {
+        return "/apart/index";
+    }
+
+}
